b9c074eb582ab19caa1cf0d9ff761833177bf144
Ishaan Ohri <ishaan99ohri@gmail.com>
2020-11-07 12:00:50 +0530

    Home route added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,14 @@ const app = express();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+    console.log('Home route');
+    res.send({
+        success: true,
+        message: 'Welcome to Multi Container App'
+    });
+});
+
 app.listen(PORT, HOST, () => {
     console.log(`Server listening on http://${HOST}:${PORT}`);
 })
